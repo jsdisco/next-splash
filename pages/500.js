64bfecda48d36142b394d026f14ebc6bae215631
jsdisco <mar18yyzz@gmail.com>
2021-07-21 11:42:44 +0200

    added custom 500

---
+++ pages/500.js
@@ -0,0 +1,11 @@
+import styles from '../styles/NotFound.module.css';
+
+const InternalServerError = () => {
+  return (
+    <div className={styles.container}>
+      <p>--- development ---</p>
+    </div>
+  );
+};
+
+export default InternalServerError;
