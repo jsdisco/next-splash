68f6cea82e92fb875713204ea4e7db828a782dc5
jsdisco <mar18yyzz@gmail.com>
2021-07-21 11:39:40 +0200

    added custom 404

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import styles from '../styles/NotFound.module.css';
+
+const NotFound = () => {
+  return (
+    <div className={styles.container}>
+      <p>--- development ---</p>
+    </div>
+  );
+};
+
+export default NotFound;
