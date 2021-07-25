6f5eb5e942d2010c41aed6439f68f7b2dca43d21
jsdisco <mar18yyzz@gmail.com>
2021-07-25 22:16:50 +0200

    small fix in Errors.js

--- components/Errors.js
+++ components/Errors.js
@@ -1,11 +1,9 @@
 export default function Errors({ errors, status = '' }) {
   return (
     <div style={{ marginBottom: '1rem' }}>
-      <p style={{ color: 'crimson' }}>Error:</p>
+      <p style={{ color: 'crimson' }}>{status} Error:</p>
       {errors.map((err, i) => (
-        <p key={i}>
-          {status} {err}
-        </p>
+        <p key={i}>{err}</p>
       ))}
     </div>
   );
