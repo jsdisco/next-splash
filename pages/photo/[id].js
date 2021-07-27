e7786744a4ac5928b4f57640d6f2efbc1cf03c6e
jsdisco <mar18yyzz@gmail.com>
2021-07-27 15:52:55 +0200

    /photo/[id]: switch from getServerSideProps to getStaticProps/Paths

--- pages/photo/[id].js
+++ pages/photo/[id].js
@@ -4,12 +4,36 @@ import Errors from '../../components/Errors';
 export default function PhotoDetailsPage({ data }) {
   return (
     <>
-      {data.errors && <Errors errors={data.errors} status={data.status} />}
-      {data.photo && <PhotoPage photo={data.photo} />}
+      {data && data.errors && (
+        <Errors errors={data.errors} status={data.status} />
+      )}
+      {data && data.photo && <PhotoPage photo={data.photo} />}
     </>
   );
 }
 
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SERVER}/api/unsplash/${params.id}`
+  );
+  const data = await res.json();
+
+  data.status = res.status;
+
+  return {
+    props: { data },
+    revalidate: 600,
+  };
+};
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
+
+/*
 export const getServerSideProps = async ({ query }) => {
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_SERVER}/api/unsplash/${query.id}`
@@ -22,3 +46,4 @@ export const getServerSideProps = async ({ query }) => {
     props: { data },
   };
 };
+*/
